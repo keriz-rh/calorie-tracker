da7457ef9d9ee25c1513dc6024447bf40fe4067e
keriz-rh <rh16042@ues.edu.sv>
2025-04-11 10:43:07 -0600

    Se agrega resumen de calorias

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer, useEffect, useMemo } from "react"
 import Form from "./components/form"
 import { activityReducer, initialState } from "./reducers/activity-reduce"
 import ActivityList from "./components/ActivityList"
+import CalorieTracker from "./components/CalorieTracker"
 
 
 function App() {
@@ -31,10 +32,8 @@ function App() {
          >
             Reiniciar App
           </button>
-
         </div>
       </header>
-
 
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
@@ -43,6 +42,15 @@ function App() {
             state={state}
           />
         </div>
+      </section>
+
+      <section className="bg-gray-800 py-10">
+        <div className="max-w-4xl mx-auto">
+        <CalorieTracker
+        activities={state.activities}
+          />
+        </div>
+
       </section>
 
       <section className="p-10 mx-auto max-w-4xl">
